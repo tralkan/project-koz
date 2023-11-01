8c0afc1d79e909091eed6501629c6a4b7764e20e
Xalkan Duarte <xalkan@pm.me>
2023-11-01 02:42:53 -0300

    deployment scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,29 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const addresses = ["0xC68d43b78b5B720b0A1392269aFaC939DDfA40EE"];
+  const [signer, otherAccount] = await ethers.getSigners();
+  console.log(`Signer address: ${signer.address}`);
+
+  const addresses = [
+    "0x1c61dE56e7b39efaCfEcE6fEDa5807dcDFBaB7c6",
+    otherAccount.address,
+  ];
   const dns = "0xC68d43b78b5B720b0A1392269aFaC939DDfA40EE";
   const ids: any[] = [];
+  const entryPoint = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 
-  const contract = await ethers.deployContract("Account", [
+  const contract = await ethers.deployContract("TyronSSIAccount", [
     addresses,
     dns,
     ids,
+    entryPoint,
   ]);
-  await contract.waitForDeployment();
+  await contract.waitForDeployment().then(async () => {
+    console.log(`Account deployed to: ${contract.target}`);
 
-  console.log(`Deployed to ${contract.target}`);
+    // const txn = await contract.initialize(signer);
+    // console.log(`Initialised at ${JSON.stringify(txn, null, 2)}`);
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ scripts/deployInit.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const addresses = ["0xC68d43b78b5B720b0A1392269aFaC939DDfA40EE"];
+  const dns = "0xC68d43b78b5B720b0A1392269aFaC939DDfA40EE";
+  const ids: any[] = [];
+
+  const contract = await ethers.deployContract("AccountInit", [
+    addresses,
+    dns,
+    ids,
+  ]);
+  await contract.waitForDeployment();
+
+  console.log(`Deployed to ${contract.target}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
